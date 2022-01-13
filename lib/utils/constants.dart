cb293d0ac3bb3b01b3cca3498de808f73297e2dd
Joe Tsui <cmtsui@gmail.com>
2022-01-13 17:51:56 +0800

    To add folder and file

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,5 @@
-const MONGO_CONN_URL = "mongodb://10.0.2.2:27017/newdb"; //localhost if not emulator
+const MONGO_CONN_URL = "mongodb://10.0.2.2:27017/newdb";
+//emulated device uses 10.0.2.2
+//actual device use localhost
+
 const USER_COLLECTION = "lightuser";
